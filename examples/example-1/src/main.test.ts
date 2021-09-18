86f1a2fa6887047a402ebf33995e4a9dcd280624
Jordan Mele <SiliconSoldier@outlook.com.au>
2021-09-18 13:56:19 +1000

    ideation on api surface

--- examples/example-1/src/main.test.ts
+++ examples/example-1/src/main.test.ts
@@ -1,6 +1,12 @@
 import { fibonacci } from "./main.js";
 import { is } from "@theory/assertions";
 
-export function fib1() {
-    is(fibonacci(2), 4);
+// wrapper needed for decoration and enabling composability (tag tests so that exports can be filtered)
+function test(testFn: unknown): unknown {
+    return testFn;
 }
+
+// t = execution context (similar to with ava), as `import('context')` cannot work reliably with async code
+export const fib1 = test(t => {
+    is(fibonacci(2), 4);
+});
